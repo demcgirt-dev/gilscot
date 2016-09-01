00a14ffc918f31a920aba173493e39390420a38d
davidmcgirt <davidmcgirt@gmail.com>
2016-09-01 17:23:47 +0000

    added bootstrap to application

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  
+  before_save { self.email = email.downcase } # before email saves to database, downcase it
+  has_secure_password # bcrypt method
+  
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 30 }
+            
+  # Regular Expressions can be tested here: [ rubular.com ]
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :email, presence: true, length: { minimum: 10, maximum: 50 }, uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+            
+end
